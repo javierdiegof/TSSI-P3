aafae3753400717b997609f494f06698de8f3fc5
Javier Diego <javier.d.fernandez7@gmail.com>
2018-09-19 21:24:26 -0500

    Miercoles 19, despues de correcciones

--- p3c1/p3c1.ino
+++ p3c1/p3c1.ino
@@ -1,18 +1,14 @@
-/*
-  This program blinks pin 13 of the Arduino (the
-  built-in LED)
-*/
-
-int ena1 = 2;
-int ena2 = 3;
-int dir1 = 5;
-int dir2 = 6;
+int PIN_ENA1 = 2;
+int PIN_ENA2 = 3;
+int PIN_DIR1 = 5;
+int PIN_DIR2 = 6;
 
 //String lec_motor_actual;
 //String lec_stat_actual;
 //String lec_dir_actual;
 
-int motor_actual = -1;
+int pin_enable = -1;
+int pin_dir    = -1;
 int stat_actual = -1;
 int dir_actual = -1;
 int check = -1;
@@ -28,10 +24,10 @@ String lectura;
 void setup()
 {
   Serial.begin(9600);
-  pinMode(ena1, OUTPUT);
-  pinMode(ena2, OUTPUT);
-  pinMode(dir1, OUTPUT);
-  pinMode(dir2, OUTPUT);
+  pinMode(PIN_ENA1, OUTPUT);
+  pinMode(PIN_ENA2, OUTPUT);
+  pinMode(PIN_DIR1, OUTPUT);
+  pinMode(PIN_DIR2, OUTPUT);
 }
 
 void loop(){
@@ -43,7 +39,7 @@ void loop(){
       //Serial.print("stat:" + lec_stat_actual + "\n");
   
       // Procesamiento del motor a escoger
-      motor_actual = motor_actualf(lectura);
+      motor_actualf(lectura);
     
       // Procesamiento del status del motor (on/off)
       stat_actual = stat_actualf(lectura);
@@ -51,9 +47,14 @@ void loop(){
       // En caso de on, procesamiento de la dirección de 
       dir_actual = dir_actualf(lectura, stat_actual);
 
-      Serial.print("valores: " + String(motor_actual) + String(stat_actual) + String(dir_actual) + "\n");
+      Serial.println("Valores a la tarjeta: ");
+      Serial.println("Pin_enable: " + String(pin_enable));
+      Serial.println("Pin_direccion: " + String(pin_dir));
+      Serial.println("Estatus: " + String(stat_actual));
+      Serial.println("Direccion: " + String(dir_actual));
+      
       // Checando que todos los datos se hayan insertado correctamente
-      check = checar(motor_actual, stat_actual, dir_actual);
+      check = checar(pin_enable, pin_dir, stat_actual, dir_actual);
 
       // Si el chequeo es incorrecto, mostramos error al usuario
       if(check == -1){
@@ -61,19 +62,27 @@ void loop(){
       }
       // Operaciones correctas prendemos las salidas del Arduino
       else if (check == 1){
-         salida_motor(motor_actual, stat_actual, dir_actual);
+         salida_motor(pin_enable, pin_dir, stat_actual, dir_actual);
       }
   }
 }
 
 
-int motor_actualf(String lectura){
-    String lec_motor_actual;
-    lec_motor_actual = lectura.substring(0,2);
-    if(lec_motor_actual.substring(0,1).equals("A")){
-      return lec_motor_actual.substring(1).toInt() + 1; // Incremento para pin
+void motor_actualf(String lectura){
+    char letra;
+    char num;
+    letra = lectura.charAt(0);
+    num   = lectura.charAt(1);
+    if(letra == 'A'){
+      if(num == '1'){
+        pin_enable = PIN_ENA1;
+        pin_dir    = PIN_DIR1;
+      }
+      else if (num == '2'){
+        pin_enable = PIN_ENA2;
+        pin_dir    = PIN_DIR2;  
+      }
     }
-    return ERROR;
 }
 
 int stat_actualf(String lectura){
@@ -108,22 +117,22 @@ int dir_actualf(String lectura, int stat_actual){
    return -ERROR;
 }
 
-int checar(int motor_actual, int stat_actual, int dir_actual){
-   if(((motor_actual != 2) && (motor_actual != 3)) || (stat_actual == -1) || (dir_actual == -1)){
+int checar(int pin_enable,  int pin_dir, int stat_actual, int dir_actual){
+   if(((pin_enable != PIN_ENA1) && (pin_enable != PIN_ENA2)) || (stat_actual == -1) || (dir_actual == -1)){
       return -ERROR;
    }
    return 1;
 }
 
-int salida_motor(int motor_actual, int stat_actual, int dir_actual){
+int salida_motor(int pin_enable, int pin_dir, int stat_actual, int dir_actual){
    for(int i = 0; i < 500; i++){
       // Motor prendido
      if(stat_actual == PRENDIDO){
-        digitalWrite(motor_actual, PRENDIDO);
-        digitalWrite(motor_actual+3, dir_actual);
+        digitalWrite(pin_enable, PRENDIDO);
+        digitalWrite(pin_dir, dir_actual);
       }
       else{
-         digitalWrite(motor_actual, APAGADO);
+        digitalWrite(pin_enable, APAGADO);
       }
    }
 }
--- p3c2/p3c2.ino
+++ p3c2/p3c2.ino
@@ -3,18 +3,18 @@
   built-in LED)
 */
 
-int ena1 = 2;
-int ena2 = 3;
-int dir1 = 5;
-int dir2 = 6;
+int PIN_ENA1 = 2;
+int PIN_ENA2 = 3;
+int PIN_DIR1 = 5;
+int PIN_DIR2 = 6;
 
 //String lec_motor_actual;
 //String lec_stat_actual;
 //String lec_dir_actual;
 
-int motor_actual = -1;
+int pin_enable = -1;
+int pin_dir = -1;
 int stat_actual = -1;
-int dir_actual = -1;
 int val_dir_actual = -1000;
 int check = -1;
 
@@ -29,10 +29,10 @@ String lectura;
 void setup()
 {
   Serial.begin(9600);
-  pinMode(ena1, OUTPUT);
-  pinMode(ena2, OUTPUT);
-  pinMode(dir1, OUTPUT);
-  pinMode(dir2, OUTPUT);
+  pinMode(PIN_ENA1, OUTPUT);
+  pinMode(PIN_ENA2, OUTPUT);
+  pinMode(PIN_DIR1, OUTPUT);
+  pinMode(PIN_DIR2, OUTPUT);
 }
 
 void loop(){
@@ -46,17 +46,23 @@ void loop(){
       //Serial.print("stat:" + lec_stat_actual + "\n");
   
       // Procesamiento del motor a escoger
-      motor_actual = motor_actualf(lectura);
+      motor_actualf(lectura);
     
       // Procesamiento del status del motor (on/off)
       stat_actual = stat_actualf(lectura);
     
       // En caso de on, procesamiento de la dirección de 
-      dir_actual = dir_actualf(lectura, stat_actual);
+      val_dir_actual = dir_actualf(lectura, stat_actual);
 
-      Serial.print("valores: " + String(motor_actual) + " , " +  String(stat_actual) + " , " + String(dir_actual) + "\n\r");
+      Serial.println("Valores a la tarjeta: ");
+      Serial.println("Pin_enable: " + String(pin_enable));
+      Serial.println("Pin_direccion: " + String(pin_dir));
+      Serial.println("Estatus: " + String(stat_actual));
+      Serial.println("Direccion: " + String(val_dir_actual));
+
+      
       // Checando que todos los datos se hayan insertado correctamente
-      check = checar(motor_actual, stat_actual, dir_actual);
+      check = checar(pin_enable, pin_dir, stat_actual, val_dir_actual);
 
       // Si el chequeo es incorrecto, mostramos error al usuario
       if(check == -1){
@@ -64,19 +70,27 @@ void loop(){
       }
       // Operaciones correctas prendemos las salidas del Arduino
       else if (check == 1){
-         salida_motor(motor_actual, stat_actual, dir_actual);
+         salida_motor(pin_enable, pin_dir, stat_actual, val_dir_actual);
       }
   }
 }
 
 
-int motor_actualf(String lectura){
-    String lec_motor_actual;
-    lec_motor_actual = lectura.substring(0,2);
-    if(lec_motor_actual.substring(0,1).equals("A")){
-      return lec_motor_actual.substring(1).toInt() + 1; // Incremento para pin
+void motor_actualf(String lectura){
+    char letra;
+    char num;
+    letra = lectura.charAt(0);
+    num   = lectura.charAt(1);
+    if(letra == 'A'){
+      if(num == '1'){
+        pin_enable = PIN_ENA1;
+        pin_dir    = PIN_DIR1;
+      }
+      else if (num == '2'){
+        pin_enable = PIN_ENA2;
+        pin_dir    = PIN_DIR2;  
+      }
     }
-    return ERROR;
 }
 
 int stat_actualf(String lectura){
@@ -93,17 +107,11 @@ int dir_actualf(String lectura, int stat_actual){
   if(stat_actual == PRENDIDO){
       lec_dir_actual = lectura.substring(9);
       if(lec_dir_actual.substring(0,1).equals("+")){
-         //Serial.print("direccion positiva: " + lec_dir_actual.substring(1) + "\n");
-         Serial.print("subcadena: " + String(lec_dir_actual.substring(1)) + "\n");
-         Serial.print("convertido: " + String(lec_dir_actual.substring(1).toInt()) + "\n");
          val_dir_actual = lec_dir_actual.substring(1).toInt();
          val_dir_actual = val_dir_actual + (int)127;
          Serial.print("magnitud: " + String(val_dir_actual) + "\n");
       }
       else if(lec_dir_actual.substring(0,1).equals("-")){
-         Serial.print("subcadena: " + String(lec_dir_actual.substring(1)) + "\n");
-         Serial.print("convertido: " + String(lec_dir_actual.substring(1).toInt() + 127) + "\n");
-         Serial.print("direccion negativa: " + lec_dir_actual.substring(1) + "\n");
          val_dir_actual = lec_dir_actual.substring(1).toInt();
          val_dir_actual = (0 - val_dir_actual) + (int)127;
          Serial.print("magnitud: " + String(val_dir_actual) + "\n");
@@ -115,22 +123,23 @@ int dir_actualf(String lectura, int stat_actual){
       
       return val_dir_actual;
    }
-   return -ERROR;
+   return ERROR;
 }
 
-int checar(int motor_actual, int stat_actual, int dir_actual){
-   if(((motor_actual != 2) && (motor_actual != 3)) || (stat_actual != PRENDIDO) || (dir_actual == -1)){
+int checar(int pin_enable,  int pin_dir, int stat_actual, int val_dir_actual){
+   if(((pin_enable != PIN_ENA1) && (pin_enable != PIN_ENA2)) || (stat_actual == -1) || (val_dir_actual > 254) || (val_dir_actual < 0)){
       return ERROR;
    }
    return 1;
 }
 
-int salida_motor(int motor_actual, int stat_actual, int dir_actual){
+int salida_motor(int pin_enable, int pin_dir, int stat_actual, int val_dir_actual){
    for(int i = 0; i < 500; i++){
       // Motor prendido
      if(stat_actual == PRENDIDO){
-        digitalWrite(motor_actual, PRENDIDO);
-        analogWrite(motor_actual+3, dir_actual);
+        digitalWrite(pin_enable, PRENDIDO);
+        analogWrite(pin_dir, val_dir_actual);
       }
    }
 }
+
